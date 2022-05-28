031235f1a2013aa5b31ac2dbc01b8d40877e4227
agusolmos <agustinolmos26@gmail.com>
2022-05-28 17:09:31 -0300

    subiendo archivos

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Inicio } from "./components/Pages/Inicio";
+import { About } from "./components/Pages/quien";
+import { Blog } from "./components/Pages/producto";
+import { Contact } from "./components/Pages/Contacto";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <NavBar />
+
+        <div className="pages">
+          <Switch>
+            <Route exact path="/" component={Inicio} />
+            <Route path="/quien" component={quien} />
+            <Route path="/producto" component={producto} />
+            <Route path="/Contacto" component={Contacto} />
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
