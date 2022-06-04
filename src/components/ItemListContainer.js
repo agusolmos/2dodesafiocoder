e5458a042e366883849e2837248e81b22c295e7c
agusolmos <95132350+agusolmos@users.noreply.github.com>
2022-06-04 17:07:03 -0300

    modificaciones para 3er desafio

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,52 @@
+import ItemCount from './CountContainer';
+import ItemList from './ItemList';
+import Item from './Item';
+import jsonpack from './data.json';
+import React, {useState,useEffect} from 'react';
+
+
+           // <ItemCount product_name='Paquete de Zanahorias Deshidratas' stock={5} initial={1} />
+           //     <ItemCount product_name='Paquete de Berenjena Deshidratas' stock={9} initial={1} />
+
+        // <Item jsonpack={test2} />
+
+
+
+
+
+
+const ItemListContainer = ({name}) => {
+    const[item,setItems]=useState([])
+    const call = new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(jsonpack)
+        },2000)
+    })
+
+    call.then(response=> {
+        setItems(response)
+    })
+
+
+
+    return (
+
+       <div name="test">
+
+
+
+    <div class="p-3 mb-2 bg-dark text-white">
+        {name}
+
+        <ItemList items={item}/>
+
+       </div>
+
+
+
+           </div>
+   )
+}
+
+
+export default ItemListContainer;
